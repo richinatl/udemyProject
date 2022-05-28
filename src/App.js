a35625db2ecbdbd60ca36b5573d6a84ae400f66b
Richard Murphy <richinatl@gmail.com>
2022-05-27 20:06:18 -0400

    React Travel App

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
